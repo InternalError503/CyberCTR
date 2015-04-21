a6483fbfe7722870d283d4ed80277fc62901079a
toady <support@8pecxstudios.com>
2015-04-21 18:39:51 +1000

    Remove the last coma from the array string if present.

--- distribution/bundles/CTR@8pecxstudios.com/content/overlay.js
+++ distribution/bundles/CTR@8pecxstudios.com/content/overlay.js
@@ -4723,7 +4723,7 @@ classicthemerestorerjs.ctr = {
 					/*AGENT_SHEET*/\
 					@namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);\
 					@-moz-document url(chrome://browser/content/browser.xul) {\
-						'+elementData+'{\
+						'+elementData.replace(/,\s*$/, "")+'{\
 							display:none!important;\
 						}\
 					}\
