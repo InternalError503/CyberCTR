f1f4c0ceb7188d7787a7e1d2fed87864500f250b
toady <support@8pecxstudios.com>
2015-05-13 12:26:48 +1000

    Passed CTR 1.3.2 b3 features over to CyberCTR

--- distribution/bundles/CTR@8pecxstudios.com/content/options.js
+++ distribution/bundles/CTR@8pecxstudios.com/content/options.js
@@ -349,6 +349,9 @@ classicthemerestorerjso.ctr = {
 	if (this.appversion > 37) {
 	  document.getElementById('ctraddon_pw_bmarkoinpw').style.visibility = 'collapse';
 	}
+	if (this.appversion < 40) {
+	  document.getElementById('ctraddon_pw_pocketgb').style.visibility = 'collapse';
+	}
 
 	function PrefListener(branch_name, callback) {
 	  // Keeping a reference to the observed preference branch or it will get
