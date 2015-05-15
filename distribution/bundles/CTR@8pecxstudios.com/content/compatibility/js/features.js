e15c858eadd956ade79c0c8d7f068db43b1de133
toady <support@8pecxstudios.com>
2015-05-15 12:26:21 +1000

    Added: Localization for features & information UI elements.

--- distribution/bundles/CTR@8pecxstudios.com/content/compatibility/js/features.js
+++ distribution/bundles/CTR@8pecxstudios.com/content/compatibility/js/features.js
@@ -9,7 +9,7 @@ if (!cyberctrFeatures) {
 };
 
 cyberctrFeatures = {
-
+			getMessage: Services.strings.createBundle("chrome://classic_theme_restorer/locale/features.file"),
     initialize_features: function() {
 
         if (!Services.prefs.getBoolPref("extensions.classicthemerestorer.features.firstrun")) {
@@ -22,7 +22,27 @@ cyberctrFeatures = {
 		if (Services.appinfo.name.toLowerCase() === "Firefox".toLowerCase()) {
 			document.getElementById("up-check").hidden = true;
 			document.getElementById("up-check-oc").hidden = true;
-		}	
+		}
+		
+		//Localize UI elements.
+		document.getElementById("btn-check-update").textContent =cyberctrFeatures.i18n("btn-check-update");
+		document.getElementById("btn-Documentation").textContent =cyberctrFeatures.i18n("btn-Documentation");
+		document.getElementById("btn-faq").textContent =cyberctrFeatures.i18n("btn-faq");
+		document.getElementById("btn-support-forums").textContent =cyberctrFeatures.i18n("btn-support-forums");
+		document.getElementById("btn-contact-us").textContent =cyberctrFeatures.i18n("btn-contact-us");
+		//Off canvas
+		document.getElementById("btn-check-update-oc").textContent =cyberctrFeatures.i18n("btn-check-update");
+		document.getElementById("btn-Documentation-oc").textContent =cyberctrFeatures.i18n("btn-Documentation");
+		document.getElementById("btn-faq-oc").textContent =cyberctrFeatures.i18n("btn-faq");
+		document.getElementById("btn-support-forums-oc").textContent =cyberctrFeatures.i18n("btn-support-forums");
+		document.getElementById("btn-contact-us-oc").textContent =cyberctrFeatures.i18n("btn-contact-us");
+		//Updates
+		document.getElementById("msg-firstrun-welcome").textContent =cyberctrFeatures.i18n("msg-firstrun-welcome");
+		document.getElementById("msg-new-version-blob").createTextNode =cyberctrFeatures.i18n("msg-new-version-blob");
+		document.getElementById("url-new-download").setAttribute('href', 'https://8pecxstudios.com/Forums/viewtopic.php?f=6&t=475#download');
+		document.getElementById("url-new-notes").setAttribute('href', 'https://8pecxstudios.com/Forums/viewtopic.php?f=6&t=475#release-notes');
+		document.getElementById("msg-no-new-version").textContent =cyberctrFeatures.i18n("msg-no-new-version");
+
     },
 	
 	updateCheck: function (manual) {
@@ -137,6 +157,15 @@ cyberctrFeatures = {
             //Show error
             Components.utils.reportError(eve);
         }
+	},	
+	
+	i18n: function(message_id){
+		
+		if (!document.getElementById(message_id)){
+			return;
+		}	
+		
+		return this.getMessage.GetStringFromName(message_id);
 	},	
 
     get_localized_document: function() {
