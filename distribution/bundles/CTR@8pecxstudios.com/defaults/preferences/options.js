6082c651611932c2a29cc74f3fca9e1af1191051
toady <support@8pecxstudios.com>
2016-01-12 18:11:01 +1000

    Bumped version

--- distribution/bundles/CTR@8pecxstudios.com/defaults/preferences/options.js
+++ distribution/bundles/CTR@8pecxstudios.com/defaults/preferences/options.js
@@ -321,7 +321,7 @@ pref("extensions.classicthemerestorer.hidexulfilter", "");
 pref("extensions.classicthemerestorer.animatedlwtthemes", false);
 
 //CyberCTR version
-pref("extensions.classicthemerestorer.version", "1.4.8");
+pref("extensions.classicthemerestorer.version", "1.4.9");
 
 //CyberCTR Pre-set Configuration
 pref("extensions.classicthemerestorer.ctrpref.firstrun", true);
