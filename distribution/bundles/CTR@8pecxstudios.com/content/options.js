d58ece0f192e3a4a45b861c0c787961ccead7968
toady <support@8pecxstudios.com>
2016-02-21 10:13:29 +1000

    Passed CTR 1.4.8b1 features over to CyberCTR

--- distribution/bundles/CTR@8pecxstudios.com/content/options.js
+++ distribution/bundles/CTR@8pecxstudios.com/content/options.js
@@ -428,6 +428,7 @@ classicthemerestorerjso.ctr = {
 
 	if (this.appversion < 44) {
 	  document.getElementById('ctraddon_pw_altalertbox').style.visibility = 'collapse';
+	  document.getElementById('ctraddon_pw_urlbardropm2').style.visibility = 'collapse';
 	}
 
 
@@ -670,7 +671,6 @@ classicthemerestorerjso.ctr = {
 	// update sub settings
 	this.ctrpwAppbuttonextra(this.prefs.getCharPref("appbutton"),false);
 	this.ctrpwAppbuttonColorExtra(this.prefs.getCharPref("appbuttonc"));
-	this.ctrpwTabEmptyFavicon(this.prefs.getBoolPref("emptyfavicon2"));
 	this.ctrpwCtrOldSearch(this.prefs.getBoolPref("ctroldsearch"));
 	this.ctrpwFaviconextra(this.prefs.getBoolPref("faviconurl"));
 	this.ctrpwBFextra(this.prefs.getBoolPref("backforward"));
@@ -684,6 +684,8 @@ classicthemerestorerjso.ctr = {
 	this.ctrpwNavBarPadding(this.prefs.getBoolPref("navbarpad"));
 	this.ctrpwLocationSearchbarSize(this.prefs.getBoolPref("lbsbsize"));
 	this.ctrpwCompactAddonList(this.prefs.getBoolPref("am_compact"));
+	this.ctrpwUnsortBM(this.prefs.getBoolPref("bmbunsortbm"));
+	this.ctrpwHideUrlStopRel(this.prefs.getBoolPref("hideurelstop"));
 	
 	var closetab_value = this.prefs.getCharPref("closetab");
   
@@ -906,10 +908,6 @@ classicthemerestorerjso.ctr = {
 	document.getElementById('ctraddon_padlock_extra').disabled = which;
   },
   
-  ctrpwTabEmptyFavicon: function(which) {
-	document.getElementById('ctraddon_pw_tab_emptyfavicon').disabled = which;
-  },
-  
   ctrpwBFextra: function(which) {
 	var itemvis = 'collapse';
 	
@@ -1020,6 +1018,32 @@ classicthemerestorerjso.ctr = {
 	
     document.getElementById('ctraddon_pw_am_compact2').disabled = which;
 	document.getElementById('ctraddon_pw_am_compact2').style.visibility = itemvis;
+  },
+  
+  ctrpwUnsortBM: function(which) {
+	var itemvis = 'collapse';
+	
+    if(which==true) {
+	  which=false; itemvis = 'visible';
+	} else {
+	  which=true; itemvis = 'collapse';
+	}
+	
+    document.getElementById('ctraddon_pw_bmbunsortbm2').disabled = which;
+	document.getElementById('ctraddon_pw_bmbunsortbm2').style.visibility = itemvis;
+  },
+  
+  ctrpwHideUrlStopRel: function(which) {
+	var itemvis = 'collapse';
+	
+    if(which==true) {
+	  which=false; itemvis = 'visible';
+	} else {
+	  which=true; itemvis = 'collapse';
+	}
+	
+    document.getElementById('ctraddon_pw_hideurelstop2').disabled = which;
+	document.getElementById('ctraddon_pw_hideurelstop2').style.visibility = itemvis;
   },
   
   ctrpwCtrOldSearch: function(which) {
@@ -1334,7 +1358,7 @@ classicthemerestorerjso.ctr = {
 	this.prefs.setCharPref("findbar",'findbar_bottoma');
 	this.prefs.setBoolPref("hideprivmask",true);
 	this.prefs.setBoolPref("cpanelmenus",true);
-	this.prefs.setBoolPref("emptyfavicon",true);
+	this.prefs.setCharPref("emptyfavico_t",'emptyfavico_t_dot');
 	this.prefs.setBoolPref("hidezoomres",true);
 	this.prefs.setBoolPref("faviconurl",true);
 	this.prefs.setBoolPref("bmanimation",true);
