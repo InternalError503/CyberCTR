f80810594a3fb4ee25575bcae13b0c622cdb202e
toady <support@8pecxstudios.com>
2016-03-28 21:38:22 +1000

    Bumped version in preferences

--- distribution/bundles/CTR@8pecxstudios.com/defaults/preferences/options.js
+++ distribution/bundles/CTR@8pecxstudios.com/defaults/preferences/options.js
@@ -339,7 +339,7 @@ pref("extensions.classicthemerestorer.loadcustomcss", false);
 pref("extensions.classicthemerestorer.customcssurl", "");
 
 //CyberCTR version
-pref("extensions.classicthemerestorer.version", "1.5.1");
+pref("extensions.classicthemerestorer.version", "1.5.2");
 
 //CyberCTR Pre-set Configuration
 pref("extensions.classicthemerestorer.ctrpref.firstrun", true);
