d5e5052308ee2db63bbe6c3935ff8dd7167caae0
toady <support@8pecxstudios.com>
2016-06-11 20:32:49 +1000

    Minor improvements to build.py
    Since we don't support windows xp we don't need to pack the css for it.
    
    Few minor adjustments for easier code readability.
    
    Removed: Documentation path from resources.

--- CTR@8pecxstudios.com/build.py
+++ CTR@8pecxstudios.com/build.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
 # Imports
-import os
-import re
-import sys
-
+import os, re, sys
 from glob import glob
 from zipfile import ZipFile, ZIP_STORED, ZIP_DEFLATED
 
@@ -13,7 +10,6 @@ resources = [
 	"content/*",
 	"content/compatibility/*",
 	"content/compatibility/css/*",
-	"content/compatibility/documentation/*",
 	"content/compatibility/fonts/*",
 	"content/compatibility/images/*",
 	"content/compatibility/js/*",
@@ -23,7 +19,6 @@ resources = [
 	"content/css/mac/*",
 	"content/css/win8/*",
 	"content/css/winaero/*",
-	"content/css/winxp/*",
 	"content/images/*",
 	"content/images/abouthome/*",
 	"content/images/cctr/*",
@@ -47,12 +42,12 @@ class ZipOutFile(ZipFile):
 
 # Enumerate resource files and folders.	
 def get_files(resources):
-    for r in resources:
-        if os.path.isfile(r):
-            yield r
+    for file in resources:
+        if os.path.isfile(file):
+            yield file
             continue
-        for g in glob(r):
-            yield g
+        for name in glob(file):
+            yield name
 
 # Build XPI package.
 def buildXPI( version ):
@@ -62,11 +57,11 @@ def buildXPI( version ):
 		print("Found & removing: " + destination)
 		os.remove(destination)
 	print('Creating package please wait!')	
-	with ZipOutFile(destination) as zp:
-		for f in sorted(get_files(resources), key=str.lower):
-			compress_type = ZIP_STORED if f.endswith(".png") else ZIP_DEFLATED
-			zp.write(f, compress_type=compress_type)
-			print("Compressing: " + f)
+	with ZipOutFile(destination) as zip:
+		for file in sorted(get_files(resources), key=str.lower):
+			compress_type = ZIP_STORED if file.endswith(".png") else ZIP_DEFLATED
+			print("Compressing: " + file)
+			zip.write(file, compress_type=compress_type)		
 	return;
 
 # Since we use command line arguments we require two in total "build" & "0.0.0" (Version) for XPI package.	
@@ -76,6 +71,7 @@ if sys.argv[1] == "build":
 		os.makedirs("xpi")
 	# Send version to build XPI package.
 	buildXPI(sys.argv[2])
+	print('Build complete!')
 	sys.exit()
 else:
 	# If the above command line arguments were not set say goodbye.
