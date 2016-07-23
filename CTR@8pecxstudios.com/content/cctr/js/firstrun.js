a0d907846d91df26c51b9ec8bdd96beaa203fde2
toady <support@8pecxstudios.com>
2016-07-23 12:21:23 +1000

    Added: Clear button and improved button logic.

--- CTR@8pecxstudios.com/content/cctr/js/firstrun.js
+++ CTR@8pecxstudios.com/content/cctr/js/firstrun.js
@@ -45,6 +45,10 @@
 				document.getElementById("themeskipbutton").textContent = cyberctrFirstrun.i18n("nav-tab-skip");
 				document.getElementById("homeskipbutton").textContent = cyberctrFirstrun.i18n("nav-tab-skip");
 				document.getElementById("nav-tab-done").textContent = cyberctrFirstrun.i18n("nav-tab-done");
+				document.getElementById("menunextbuttonclear").textContent = cyberctrFirstrun.i18n("nav-tab-clear");
+				document.getElementById("themenextbuttonclear").textContent = cyberctrFirstrun.i18n("nav-tab-clear");
+				document.getElementById("homenextbuttonclear").textContent = cyberctrFirstrun.i18n("nav-tab-clear");
+				document.getElementById("nav-tab-doneclear").textContent = cyberctrFirstrun.i18n("nav-tab-clear");
 			} catch (e) {
 				throw new Error("Error document.getElementById(id) is null!");
 			}
@@ -52,7 +56,7 @@
 			// Set operating system specific features for linux.
 			// TODO: Maybe make operating system specific css.
 			try {
-				if (this.OS === "Linux"){
+				if (this.OS === "Linux") {
 					document.getElementById("appmenubutton_img").src = "images/appmenubutton_linux.jpg";
 					document.getElementById("appmenu_img").src = "images/appmenu_linux.jpg";
 					document.getElementById("classicthemestyle_img").src = "images/classicthemestyle_linux.jpg";
@@ -60,6 +64,44 @@
 				}
 			} catch (e) {
 				throw new Error("Error unable to set images for linux!");
+			}
+
+			// Setup event listeners for feature clicks.
+			try {
+				// AppMenu Tab
+				document.getElementById('appmenubuttonradio').addEventListener('click', function () {
+					document.getElementById("menunextbutton").style.display = 'inline-block';
+					document.getElementById("menunextbuttonclear").style.display = 'inline-block';
+				}, false);
+				document.getElementById('appmenuradio').addEventListener('click', function () {
+					document.getElementById("menunextbutton").style.display = 'inline-block';
+					document.getElementById("menunextbuttonclear").style.display = 'inline-block';
+				}, false);
+
+				// Tab Style
+				document.getElementById('classicthemestyleradio').addEventListener('click', function () {
+					document.getElementById("themenextbutton").style.display = 'inline-block';
+					document.getElementById("themenextbuttonclear").style.display = 'inline-block';
+				}, false);
+				document.getElementById('australisthemestyleradio').addEventListener('click', function () {
+					document.getElementById("themenextbutton").style.display = 'inline-block';
+					document.getElementById("themenextbuttonclear").style.display = 'inline-block';
+				}, false);
+
+				// Home Page Color
+				document.getElementById('homestyledarkradio').addEventListener('click', function () {
+					document.getElementById("homenextbutton").style.display = 'inline-block';
+					document.getElementById("homenextbuttonclear").style.display = 'inline-block';
+				}, false);
+				document.getElementById('homestylelightradio').addEventListener('click', function () {
+					document.getElementById("homenextbutton").style.display = 'inline-block';
+					document.getElementById("homenextbuttonclear").style.display = 'inline-block';
+				}, false);
+				document.getElementById('syncstyleradio').addEventListener('click', function () {
+					document.getElementById("nav-tab-doneclear").style.display = 'inline-block';
+				}, false);
+			} catch (e) {
+				throw new Error("Error unable to setup event listeners!");
 			}
 		},
 
@@ -70,41 +112,55 @@
 				throw new Error("Error aTab is null!");
 			}
 		},
-		
+
+		clearSelection: function (aName, aTab) {
+			try {
+				for (var i = 0; i < document.getElementsByName(aName).length; i++) {
+					document.getElementsByName(aName)[i].checked = false;
+				}
+				if (aTab != 'nav-tab-done') {
+					document.getElementById(aTab).style.display = 'none';
+				}
+				document.getElementById(aTab + "clear").style.display = 'none';
+			} catch (e) {
+				throw new Error("Error aName or aTab is null!");
+			}
+		},
+
 		userDone: function () {
 
-			if (Services.prompt.confirm(null, this.getMessage.GetStringFromName("change-confirm-title"),this.getMessage.GetStringFromName("change-confirm-message"))){
+			if (Services.prompt.confirm(null, this.getMessage.GetStringFromName("change-confirm-title"), this.getMessage.GetStringFromName("change-confirm-message"))) {
 				var _tabbrowser = Cc["@mozilla.org/appshell/window-mediator;1"]
-						.getService(Ci.nsIWindowMediator).getEnumerator('navigator:browser').getNext().gBrowser;
+					.getService(Ci.nsIWindowMediator).getEnumerator('navigator:browser').getNext().gBrowser;
 				var _doc = null;
 				var canCloseWindow = false;
 				for (var found = false, index = 0; index < _tabbrowser.tabContainer.childNodes.length && !found; index++) {
 					if (_tabbrowser.tabContainer.childNodes.length >= 2) {
 						canCloseWindow = true
 					}
-					_doc =  _tabbrowser.ownerDocument;
+					_doc = _tabbrowser.ownerDocument;
 				}
 				if (document.getElementById("appmenubuttonradio").checked) {
 					try {
 
-						switch (this.OS){
-							case "WINNT": 
+						switch (this.OS) {
+							case "WINNT":
 								Services.prefs.setCharPref("extensions.classicthemerestorer.appbutton", "appbutton_v2");
-							break;
-							case "Linux": 
+								break;
+							case "Linux":
 								Services.prefs.setCharPref("extensions.classicthemerestorer.appbutton", "appbutton_v1");
-							break;
+								break;
 						}
 
-							_doc.getElementById("toolbar-menubar").setAttribute('autohide', true);
+						_doc.getElementById("toolbar-menubar").setAttribute('autohide', true);
 
-					} catch (e) {}
+					} catch (e) { }
 				}
 				if (document.getElementById("appmenuradio").checked) {
 					try {
 						Services.prefs.setCharPref("extensions.classicthemerestorer.appbutton", "appbutton_off");
 						_doc.getElementById("toolbar-menubar").setAttribute('autohide', false);
-					} catch (e) {}
+					} catch (e) { }
 				}
 				if (document.getElementById("classicthemestyleradio").checked) {
 					Services.prefs.setCharPref("extensions.classicthemerestorer.tabs", "tabs_squared");
